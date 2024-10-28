8e4012b558b040404ae638bbdb4dcdb2dc8b57f2
Daryl Bacurin <darylbacurin14@gmail.com>
2024-10-28 10:08:22 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+
+function App() {
+  
+
+  return (
+    <>
+     <Router basename="/FEM-loopstudios/">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
