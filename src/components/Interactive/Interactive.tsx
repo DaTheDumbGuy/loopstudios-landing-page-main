e189f12a1cdd8dad339167971b51a7d8ecf19365
Daryl Bacurin <darylbacurin14@gmail.com>
2024-11-21 07:58:33 +0800

    Interactive Component[responsivie]

---
+++ src/components/Interactive/Interactive.tsx
@@ -0,0 +1,17 @@
+
+export default function Interactive(){
+    return(
+        <section className="b-interactive">
+            <div className="b-interactive__holder">
+                <div className="b-interactive__imgContainer">
+                </div>
+                <div className="b-interactive__body">
+                    <h2 className="b-interactive__body__title">The Leader In Interactive VR</h2>
+                    <p className="b-interactive__body__description">
+                        Founded in 2011, Loopstudioes has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import Hero from "../../components/Hero/Hero";
+import Interactive from "../../components/Interactive/Interactive";
 import Navigation from "../../components/Navigation/Navigation";
 
 export default function Home() {
@@ -7,8 +8,7 @@ export default function Home() {
       <Navigation/>
       <main>
         <Hero/>
-
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint delectus beatae quidem cupiditate labore amet voluptatum perferendis, ducimus ex distinctio qui asperiores itaque! Voluptatem culpa aliquam expedita qui accusamus ratione tenetur, rerum accusantium numquam! Nobis sint adipisci reiciendis quidem quisquam est. Numquam voluptatibus quas sed quis ullam quae est ipsa deserunt, earum, perferendis nisi voluptates voluptas! Sint tempora laborum officiis repudiandae rem laboriosam eius et corrupti, voluptate ipsum vel ducimus commodi consectetur iusto? Aliquid mollitia tempore aperiam ea illo ex doloribus hic. Quidem fugit illo commodi excepturi dolores repellendus ad. Reprehenderit sunt cum ab consectetur ea assumenda illum tempore repudiandae.
+        <Interactive/>
       </main>
     </>
   )
